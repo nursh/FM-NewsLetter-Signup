57561883b9b0e17170b95db7f8c7159673315f30
nur-sh <nsheikhmohammed01@gmail.com>
2025-07-30 19:32:11 -0400

    fix spacing issue

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const formElem = document.querySelector('form');
 const inputElem = document.querySelector('input');
 const errorElem = document.querySelector('.error-label');
 
-formElem.addEventListener('submit', (e) => {
-  e.preventDefault();
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const data = Object.fromEntries(formData);
+  window.localStorage.setItem('email', data.email);
   window.location.href = 'success.html';
-})
+}
+
+formElem.addEventListener('submit', handleSubmit)
 
 inputElem.addEventListener('invalid', (e) => {
   e.preventDefault();
